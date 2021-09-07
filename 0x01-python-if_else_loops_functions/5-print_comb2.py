dc12f8e6f32fc128e4e9d58a1899f18224bf984d
danomwa <danomwaopiyo@gmail.com>
2021-09-07 19:30:26 -0400

    corrected

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for number in range(0, 100):
-	if int != 99:
-		print('{:02d},'.format(number), end=" ")
-	else:
-		print('{}'.format(number))
+        if int != 99:
+                print('{:02d},'.format(number), end=" ")
+        else:
+                print('{}'.format(number))
